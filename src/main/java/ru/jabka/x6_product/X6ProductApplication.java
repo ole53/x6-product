3099efc1c5a9b4819d41ad3751694b3554d6772e
Олег <ole53@yandex.ru>
2025-03-29 23:03:37 +0300

    init

---
+++ src/main/java/ru/jabka/x6_product/X6ProductApplication.java
@@ -0,0 +1,13 @@
+package ru.jabka.x6_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X6ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X6ProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jabka/x6_product/X6ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jabka.x6_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
